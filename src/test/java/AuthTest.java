58f7e0f178ee8ea714841ff9822e1ef59cdf6ee2
Краснова Татьяна <redtanya@yandex.ru>
2022-08-15 16:36:44 +0300

    init commit

---
+++ src/test/java/AuthTest.java
@@ -0,0 +1,29 @@
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
+import static com.codeborne.selenide.Selenide.open;
+import static io.restassured.RestAssured.given;
+
+class AuthTest {
+    private static RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    @BeforeAll
+    static void setUpAll() {
+        open("http://localhost:9999");
+        given() // "дано"
+                .spec(requestSpec) // указываем, какую спецификацию используем
+                .body(new RegistrationDto("vasya", "password", "active")) // передаём в теле объект, который будет преобразован в JSON
+                .when() // "когда"
+                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                .then() // "тогда ожидаем"
+                .statusCode(200); // код 200 OK
+    }
+}
